5915751a1e5357ac6d4d20c3ebbf8709ee588a8e
dobryKum <zikovtm@gmail.com>
2023-06-17 14:08:49 +0200

    Initial commit

---
+++ FirstAssignment/FirstAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstAssignment
+//
+//  Created by Tsimafei Zykau on 17.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
